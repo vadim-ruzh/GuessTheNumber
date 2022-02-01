2bf1db1065ec9f0ea5bc1b9a29d548f4be8b3b71
vadim-ruzh <85361093+vadim-ruzh@users.noreply.github.com>
2022-02-01 20:42:39 +0700

    add new randomize func

--- GuessTheNumber.cpp
+++ GuessTheNumber.cpp
@@ -3,33 +3,46 @@
 #include <time.h>
 
 
-//Заполнение массива случайными неповторяющимися числами
-void GenerateKey( int array[], const int size) {
+//Заполнение массива числами из заданного мн-ва чисел,в случайно порядке
+void RandomFilling( int* array,int* alphabet,int alphabet_size, int array_size = 10) {
 
-    int mass[10] = { 0,1,2,3,4,5,6,7,8,9 };
-
-    for (int i = 9; i >= 1; i--) {
+    for (int i = alphabet_size - 1; i >= 1; i--) {
         int j = rand() % (i + 1);
-        int tmp = mass[j];
-        mass[j] = mass[i];
-        mass[i] = tmp;
+        int tmp = alphabet[j];
+        alphabet[j] = alphabet[i];
+        alphabet[i] = tmp;
     }
     
-    for (int i = 0; i <= size - 1; i++) {
-         array[i] = mass[i];
+    for (int i = 0; i <= array_size - 1; i++) {
+         array[i] = alphabet[i];
+    }
+}
+
+//Заполнение массива случайными числами
+void RandomDigits(int* array, int array_size = 10) {
+    array[0] = rand() % 10;
+    for (int i = 1; i < array_size; i++) {
+        array[i] = rand() % 10;
+        for (int j = 0; j < i; j++) {
+            if (array[j] == array[i]) {
+                i--;
+                break;
+            }
+        }
+
     }
 }
 
 //Сравнение массивов одинаковой длины
 //Проверка на однозначное соответствие массива array_2 массиву array_1
 //Подсчет количества сооствествующих элементов и элементов для которых нашлось соответсвие,но он имеет неправильную позицию
-void ComparingArrays(int* array_1,int* array_2,int &correctly, int &closely, int size = 4) {
-    for (int i = 0; i <= size-1; i++) {
+void ComparingArrays(int* array_1,int* array_2,int &correctly, int &closely,int  array_size = 4) {
+    for (int i = 0; i <= array_size - 1; i++) {
         if (array_1[i] == array_2[i])
             correctly += 1;
 
         else{
-            for (int j = 0; j <= size - 1; j++) {
+            for (int j = 0; j <= array_size - 1; j++) {
                 if (array_1[i] == array_2[j])
                     closely += 1;
             }
@@ -38,7 +51,8 @@ void ComparingArrays(int* array_1,int* array_2,int &correctly, int &closely, int
 }
 
 //Ввод числа в массив,с указанием желаемого количества символов в числе
-//По одному знаку,в каждый элемент массива
+//Заполнение по одному знаку,в один элемент массива
+//Если количество символов в числе не соответсвует заданному,возвращение 0
 int InputDigits(int* array,int quantity_of_digits = 4) {
     int quantity = pow(10, (quantity_of_digits - 1));
 
@@ -59,9 +73,9 @@ int InputDigits(int* array,int quantity_of_digits = 4) {
 }
 
 //Вывод массива в консоль
-void OutputArray(int* array, const int size) {
+void OutputArray(int* array,int array_size) {
     std::cout << "Array: ";
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < array_size; i++)
         std::cout << array[i] << ' ';
 
     std::cout << std::endl;
@@ -72,10 +86,15 @@ int main()
     const int lenght = 4;
     int decisions[lenght];
     int anwsers[lenght];
+
+    const int lenght_alphabet = 10;
+    int alphabet[lenght_alphabet] = { 0,1,2,3,4,5,6,7,8,9 };
+
     int correct = 0;
 
+
     srand(time(NULL));
-    GenerateKey(decisions, lenght);
+    RandomFilling(decisions,alphabet,lenght_alphabet,lenght);
 
     while (correct != 4) {
         int close = 0; 
